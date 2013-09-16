9f932699accc2a0ed44fe150a3332463ded24673
Sergey Kolesnik <stdword@gmail.com>
2013-09-16 16:13:14 +0400

    configs handling

--- src/init_env.py
+++ src/init_env.py
@@ -47,7 +47,25 @@ def scripts_starter(env_type, home, bash_folder):
 
 
 def configs_starter(env_type, home, bash_folder):
-	pass
+	for config_name in os.listdir(bash_folder):
+		config_path = os.path.join(bash_folder, config_name)
+		old_config_path = os.path.join(home, config_name)
+
+		if os.path.exists(old_config_path):
+			with open(old_config_path) as f1:
+				with open(config_path) as f2:
+					if f1.read().strip() != f2.read().strip():
+						save = prompt(
+							'Save a backup for "{}"? (yes/no)'.format(old_config_path),
+							default='yes',
+							validate=r'^yes|no$',
+						) == 'yes'
+						if save:
+							local('mv {0} {0}-old'.format(old_config_path))
+						else:
+							local('rm -r {}'.format(old_config_path))
+
+		local('cp {} {}'.format(config_path, old_config_path))
 
 
 def init_env(env_type, starter):
@@ -56,15 +74,15 @@ def init_env(env_type, starter):
 	bash_folder = os.path.join(home, '.bash_{}'.format(env_type))
 
 	if os.path.exists(bash_folder):
-		replace = prompt(
-			'Replace a "{}" folder? (yes/no)'.format(bash_folder),
-			default='no',
+		save = prompt(
+			'Save a backup for "{}"? (yes/no)'.format(bash_folder),
+			default='yes',
 			validate=r'^yes|no$',
 		) == 'yes'
-		if replace:
-			local('rm -r {}'.format(bash_folder))
-		else:
+		if save:
 			local('mv {0} {0}-old'.format(bash_folder))
+		else:
+			local('rm -r {}'.format(bash_folder))
 
 	env = get_env_path()
 	env_folder = os.path.join(env, env_type)
